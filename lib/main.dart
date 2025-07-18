6ecd75e91aabf4ea6a130f8c3558addf26e81f2e
javaExactus <romulo.neves@exactus.com.br>
2025-07-18 13:25:22 -0300

    ajuste projeto inicial

--- lib/main.dart
+++ lib/main.dart
@@ -5,65 +5,83 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.red, width: 100, height: 100),
-                Container(color: Colors.blue, width: 50, height: 50),
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.blue, width: 100, height: 100),
-                Container(color: Colors.red, width: 50, height: 50),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(color: Colors.cyan, width: 50, height: 50),
-                Container(color: Colors.pinkAccent, width: 50, height: 50),
-                Container(color: Colors.purple, width: 50, height: 50),
-              ],
-            ),
-            Container(
-              color: Colors.amber,
-              width: 300,
-              height: 30,
-              child: Text(
-                'Texto Romulo',
-                style: TextStyle(color: Colors.black, fontSize: 28),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue, // Cor de fundo
-              ),
-              onPressed: () {
-                print('Vc clicou no botão kkkk');
-              },
-              child: Text('Aperte o botão', style: TextStyle(fontSize: 24),),
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter: Primeiros Passos'),
         ),
+        body: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.blue,
+                height: 100,
+                width: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.cyan,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.pinkAccent,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.purple,
+                    height: 50,
+                    width: 50,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.amber,
+                height: 30,
+                width: 300,
+                child: const Text(
+                  'Texto Romulo', // palavra aleatoria
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('Você apertou o Botão!');
+                },
+                child: const Text('Aperte o botão!!'),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          print('Você apertou o FAB!');
+        },child: const Icon(Icons.add),),
       ),
     );
   }
